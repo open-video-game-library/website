a4cf7c9fe9c2bbddeaa9529b39c683ebf4e83c1f
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2022-12-09 13:46:55 +0900

    ContactをGoogleフォームに変更

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,87 +1,16 @@
-<script lang="ts" setup>
-import { mdiSend } from "@mdi/js"
-
-const valid = ref(true)
-const isSending = ref(false)
-
-const formInfo = {
-    action: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScpDBuQV0ca-PTrxrzm4QniH_u5_wF-3SGEHx1FT3SVJGbpqg/formResponse",
-    senderName: "entry.1143921380",
-    senderEmail: "entry.2040321557",
-    message: "entry.752954745",
-}
-const formParams = reactive({
-    senderName: "",
-    senderEmail: "",
-    message: ""
-})
-let { senderName, senderEmail, message } = toRefs(formParams)
-
-const submit = () => {
-    isSending.value = true
-    const submitParams = new FormData()
-    const CORS_PROXY = "https://cors-anywhere-keitalab.herokuapp.com/"
-    const GOOGLE_FORM_ACTION = formInfo.action
-    submitParams.append("senderName", senderName.value)
-    submitParams.append("senderEmail", senderEmail.value)
-    submitParams.append("message", message.value)
-    useFetch((CORS_PROXY + GOOGLE_FORM_ACTION), {
-        method: "POST",
-        body: submitParams
-    }).then(response => {
-        console.log(response)
-        navigateTo({
-            path: '/contact/thanks',
-            query: {
-                name: senderName.value,
-                email: senderEmail.value,
-                message: message.value
-            }
-        })
-    })
-}
-</script>
-
 <template>
     <v-container class="py-16">
         <h1>Contact</h1>
-        <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-        >
-            <v-text-field
-                v-model="senderName"
-                :counter="50"
-                :rules="[v => !!v || 'Name is required', v => (v && v.length <= 50) || 'Name must be less than 50 characters']"
-                label="Name*"
-                required
-                class="my-8"
-            ></v-text-field>
-
-            <v-text-field
-                v-model="senderEmail"
-                :rules="[v => !!v || 'E-mail is required', v => /.+@.+\..+/.test(v) || 'E-mail must be valid']"
-                label="E-mail*"
-                required
-                class="my-8"
-            ></v-text-field>
-
-            <v-textarea 
-                v-model="message"
-                label="Message*"
-                class="my-8"
-                :rules="[v => !!v || 'Message is required']"
-            ></v-textarea>
-
-            <v-btn
-                :disabled="!valid"
-                color="primary"
-                class="mb-8"
-                @click="submit"
-                :appendIcon="mdiSend"
-                :loading="isSending"
-            >Send</v-btn>
-        </v-form>
+        <p class="page-subdesc">
+            オープンビデオゲームライブラリに興味を持っていただきありがとうございます。今後、研究者にとってより良いものに改良していくために、ご意見・ご要望をお聞かせください。共同研究、依頼などについてのご連絡も承っております。
+            <br />
+            Thank you for your interest in the Open Video Game Library! Please let us know your opinions and requests, so that
+            we continue to develop it to make it even better for you.
+        </p>
+        <div style="width: 640px; margin: 0 auto;">
+            <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLScpDBuQV0ca-PTrxrzm4QniH_u5_wF-3SGEHx1FT3SVJGbpqg/viewform?embedded=true"
+                width="640" height="880" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+        </div>
     </v-container>
 </template>
