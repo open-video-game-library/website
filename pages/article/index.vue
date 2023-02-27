f5bba3308a6d18e56af4c1c528a80bd72b206d4c
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-02-28 02:16:08 +0900

    style: article and contact

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -1,18 +1,22 @@
 <template>
-    <v-container>
-        <h2>記事一覧</h2>
-        <ContentList path="/article" v-slot="{ list }">
-            <div v-for="article in list" :key="article._path">
-                <ArticleCard
-                    :path="article._path"
-                    :title="article.title"
-                    :description="article.description"
-                    :thumbnail="article.thumbnail"
-                    :created_at="article.created_at"
-                    :updated_at="article.updated_at"
-                    class="my-8"
-                />
-            </div>
-        </ContentList>
-    </v-container>
+    <div>
+        <section class="content-wrapper">
+            <v-container class="content-container">
+                <h2>記事一覧</h2>
+                <ContentList path="/article" v-slot="{ list }">
+                    <div v-for="article in list" :key="article._path">
+                        <ArticleCard
+                            :path="article._path"
+                            :title="article.title"
+                            :description="article.description"
+                            :thumbnail="article.thumbnail"
+                            :created_at="article.created_at"
+                            :updated_at="article.updated_at"
+                            class="my-8"
+                        />
+                    </div>
+                </ContentList>
+            </v-container>
+        </section>
+    </div>
 </template>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,16 +1,21 @@
 <template>
-    <v-container class="py-16">
-        <h1>Contact</h1>
-        <p class="page-subdesc">
-            オープンビデオゲームライブラリに興味を持っていただきありがとうございます。今後、研究者にとってより良いものに改良していくために、ご意見・ご要望をお聞かせください。共同研究、依頼などについてのご連絡も承っております。
-            <br />
-            Thank you for your interest in the Open Video Game Library! Please let us know your opinions and requests, so that
-            we continue to develop it to make it even better for you.
-        </p>
-        <div style="width: 640px; margin: 0 auto;">
-            <iframe
-                src="https://docs.google.com/forms/d/e/1FAIpQLScpDBuQV0ca-PTrxrzm4QniH_u5_wF-3SGEHx1FT3SVJGbpqg/viewform?embedded=true"
-                width="640" height="880" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
-        </div>
-    </v-container>
+    <div>
+        <section class="content-wrapper">
+            <v-container class="content-container">
+                <h2>お問い合わせ</h2>
+                <aside>
+                    オープンビデオゲームライブラリに興味を持っていただきありがとうございます。今後、研究者にとってより良いものに改良していくために、ご意見・ご要望をお聞かせください。共同研究、依頼などについてのご連絡も承っております。
+                </aside>
+                <aside>
+                    Thank you for your interest in the Open Video Game Library! Please let us know your opinions and requests, so that
+                    we continue to develop it to make it even better for you.
+                </aside>
+                <div style="width: 640px; margin: 0 auto;">
+                    <iframe
+                        src="https://docs.google.com/forms/d/e/1FAIpQLScpDBuQV0ca-PTrxrzm4QniH_u5_wF-3SGEHx1FT3SVJGbpqg/viewform?embedded=true"
+                        width="640" height="880" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+                </div>
+            </v-container>
+        </section>
+    </div>
 </template>
