f091a96340f7427d920d8eb7db45f55622dfd682
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-03-02 14:36:42 +0900

    feat: add loading indicator

--- app.vue
+++ app.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <NuxtLayout>
-            <NuxtPage />
-        </NuxtLayout>
-    </div>
+    <NuxtLayout>
+        <NuxtLoadingIndicator
+            color="white"
+            :height="2"
+            :duration="5000"
+            :throttle="200"
+        />
+        <NuxtPage />
+    </NuxtLayout>
 </template>
