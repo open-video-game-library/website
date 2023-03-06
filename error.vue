5cfdb2cbd2b4b283591fba16942579813bad9a25
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-03-06 11:21:22 +0900

    feat: add error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<script setup>
+const error = useError()
+</script>
+
+<template>
+    <NuxtLayout>
+        <NuxtLoadingIndicator color="white" :height="2" :duration="5000" :throttle="200" />
+        <section class="content-wrapper">
+            <v-container class="content-container">
+                <h2>エラーが発生しました。リロードするか、トップページに戻ってやり直してください。</h2>
+                <p class="my-4">Status code: {{ error.statusCode }}</p>
+                <p class="my-4">Message: {{ error.statusMessage }}</p>
+                <v-btn href="https://openvideogame.cc" color="primary" class="my-4">トップページに戻る</v-btn>
+            </v-container>
+        </section>
+    </NuxtLayout>
+</template>
---
+++ pages/404.vue
@@ -0,0 +1,14 @@
+<script setup>
+const error = useError()
+</script>
+
+<template>
+    <section class="content-wrapper">
+        <v-container class="content-container">
+            <h2>お探しのページは存在しません。</h2>
+            <p class="my-4">Status code: {{ error.statusCode }}</p>
+            <p class="my-4">Message: {{ error.statusMessage }}</p>
+            <v-btn href="https://openvideogame.cc" color="primary" class="my-4">トップページに戻る</v-btn>
+        </v-container>
+    </section>
+</template>
