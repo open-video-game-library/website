61d2c28a18cba726fa2ac85faee2b6c572106a24
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-03-06 15:33:46 +0900

    feat: navigation drawer

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -9,37 +9,78 @@ import {
 
 import { useDisplay } from 'vuetify'
 const { xs } = useDisplay()
+
+const logoImg = new URL('../assets/image/logo_white.png', import.meta.url).href
+
+const headerLinks = [
+    {
+        name: "About",
+        icon: mdiInformation,
+        to: "/"
+    },
+    {
+        name: "Open Video Game",
+        icon: mdiController,
+        to: "/game"
+    },
+    {
+       name: "Tool",
+        icon: mdiHammerWrench,
+        to: "/tool"
+    },
+    {
+        name: "Article",
+        icon: mdiFileDocument,
+        to: "/article"
+    },
+    {
+        name: "Contact",
+        icon: mdiEmail,
+        to: "/contact"
+    }
+]
+
+const showDrawer = ref(false)
 </script>
 
 <template>
     <v-app-bar color="primary">
         <template v-slot:prepend>
             <v-img
-                src="https://res.cloudinary.com/dz1tqxdzg/image/upload/v1666456733/opengame-info/logos/logo_white_aq5j4v.png"
+                :src="logoImg"
                 width="100"
             ></v-img>
         </template>
         <template v-slot:append v-if="!xs">
-            <v-btn
-                :prependIcon="mdiInformation"
-                to="/"
-            >About</v-btn>
-            <v-btn
-                to="/game"
-                :prependIcon="mdiController"
-            >Open Video Game</v-btn>
-            <v-btn
-                to="/tool"
-                :prependIcon="mdiHammerWrench"
-            >Tool</v-btn>
-            <v-btn
-                to="/article"
-                :prependIcon="mdiFileDocument"
-            >Article</v-btn>
-            <v-btn
-                to="/contact"
-                :prependIcon="mdiEmail"
-            >Contact</v-btn>
+            <v-btn v-for="link in headerLinks"
+                :key="link.name"
+                :prependIcon="link.icon"
+                :to="link.to"
+            >{{ link.name }}</v-btn>
         </template>
+
+        <v-app-bar-nav-icon
+            v-if="xs"
+            variant="text"
+            @click.stop="showDrawer = !showDrawer"
+        ></v-app-bar-nav-icon>
     </v-app-bar>
+
+    <v-navigation-drawer
+        v-if="xs"
+        color="primary"
+        v-model="showDrawer"
+        location="right"
+        temporary
+    >
+        <v-list>
+            <v-list-item v-for="link in headerLinks" :key="link.name">
+                <v-btn
+                    color="primary"
+                    :prependIcon="link.icon"
+                    :to="link.to"
+                >{{ link.name }}</v-btn>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
 </template>
