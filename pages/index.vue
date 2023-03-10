ae2e3cc46967a3a1bbc0b6b8b025dd6dd96f62bc
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-03-10 17:39:23 +0900

    fix: import config

--- pages/index.vue
+++ pages/index.vue
@@ -19,22 +19,22 @@ const canImg = [
 /**
  * opengame基本情報DBのスプレッドシートから、MemberとPublicationのデータを読み込む
  */
-// const { data: memberData } = await useFetch(config.INTERNAL_DB_API, {
-//     method: "GET",
-//     query: { sheetName: "member" }
-// })
-// const { data: publicationData } = await useFetch(config.INTERNAL_DB_API, {
-//     method: "GET",
-//     query: { sheetName: "publication" }
-// })
-const { data: memberData } = await useFetch("https://script.google.com/macros/s/AKfycbxcxVKsmiwo3Pip-D_l29-XGgJiraYgVCMOLkJ2SWxEyFYS0paBbmTYAxZDm6zmsX-v8g/exec", {
+const { data: memberData } = await useFetch(config.public.INTERNAL_DB_API, {
     method: "GET",
     query: { sheetName: "member" }
 })
-const { data: publicationData } = await useFetch("https://script.google.com/macros/s/AKfycbxcxVKsmiwo3Pip-D_l29-XGgJiraYgVCMOLkJ2SWxEyFYS0paBbmTYAxZDm6zmsX-v8g/exec", {
+const { data: publicationData } = await useFetch(config.public.INTERNAL_DB_API, {
     method: "GET",
     query: { sheetName: "publication" }
 })
+// const { data: memberData } = await useFetch("https://script.google.com/macros/s/AKfycbxcxVKsmiwo3Pip-D_l29-XGgJiraYgVCMOLkJ2SWxEyFYS0paBbmTYAxZDm6zmsX-v8g/exec", {
+//     method: "GET",
+//     query: { sheetName: "member" }
+// })
+// const { data: publicationData } = await useFetch("https://script.google.com/macros/s/AKfycbxcxVKsmiwo3Pip-D_l29-XGgJiraYgVCMOLkJ2SWxEyFYS0paBbmTYAxZDm6zmsX-v8g/exec", {
+//     method: "GET",
+//     query: { sheetName: "publication" }
+// })
 
 /**
  * 上記で読み込んだMemberとPublicationのデータから、isPublicがtrueのものだけ抽出する
