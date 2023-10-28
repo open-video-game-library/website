d168fbcf6c1d0b37d2f354b2fe5fcb6a9146d25c
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-10-28 22:53:01 +0900

    feat: 一旦サンプルを実行

--- .github/actions/regularly-deploy/index.js
+++ .github/actions/regularly-deploy/index.js
@@ -1,12 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 try {
     const doc = new GoogleSpreadsheet("1HR_m-X8kJVhZ3f4FLZkp31MT8eIQLc4t5fah-Y22FsQ");
-
-    core.setOutput("result-message", JSON.stringify(doc));
     console.log("doc", JSON.stringify(doc));
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
