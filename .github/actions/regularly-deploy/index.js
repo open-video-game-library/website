f106a4a82c8e3e3192b139963fd1edfcd855ade8
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-10-29 11:08:58 +0900

    feat: axiosでスプシAPIを叩く

--- .github/actions/regularly-deploy/index.js
+++ .github/actions/regularly-deploy/index.js
@@ -1,19 +1,24 @@
 const getSheetDatas = async () => {
     const core = require('@actions/core');
     const github = require('@actions/github');
-    const { GoogleSpreadsheet } = require('google-spreadsheet');
+    const axios = require('axios');
 
     try {
-        const doc = new GoogleSpreadsheet("1HR_m-X8kJVhZ3f4FLZkp31MT8eIQLc4t5fah-Y22FsQ");
-        console.log("doc", JSON.stringify(doc));
-        // `who-to-greet` input defined in action metadata file
-        const nameToGreet = core.getInput('who-to-greet');
-        console.log(`Hello ${nameToGreet}!`);
-        const time = (new Date()).toTimeString();
-        core.setOutput("time", time);
-        // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2)
-        console.log(`The event payload: ${payload}`);
+        axios.get('https://script.google.com/macros/s/AKfycbxcxVKsmiwo3Pip-D_l29-XGgJiraYgVCMOLkJ2SWxEyFYS0paBbmTYAxZDm6zmsX-v8g/exec', {
+            params: {
+                sheetName: "openvideogame",
+            }
+        }).then((response) => {
+            // handle success
+            console.log('response', JSON.stringify(response));
+        })
+        .catch((error) => {
+            // handle error
+            console.log(error)
+        })
+        .finally(() => {
+            console.log(`always executed`);
+        });
     } catch (error) {
         core.setFailed(error.message);
     }
