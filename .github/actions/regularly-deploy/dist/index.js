7c84aaf07b53c747d57d49c8fb5baa7d06136a25
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-10-29 23:28:12 +0900

    feat: シングルエスケープ

--- .github/actions/regularly-deploy/dist/index.js
+++ .github/actions/regularly-deploy/dist/index.js
@@ -37889,7 +37889,7 @@ const getSheetDatas = async () => {
         }
     });
     const game = gameData.filter((data) => data.isPublic);
-    const escapedGame = JSON.stringify(game);
+    const escapedGame = JSON.stringify(game).replace(/'/g, "\\'");
     core.setOutput("game", escapedGame);
 };
 
--- .github/actions/regularly-deploy/index.js
+++ .github/actions/regularly-deploy/index.js
@@ -13,7 +13,7 @@ const getSheetDatas = async () => {
         }
     });
     const game = gameData.filter((data) => data.isPublic);
-    const escapedGame = JSON.stringify(game);
+    const escapedGame = JSON.stringify(game).replace(/'/g, "\\'");
     core.setOutput("game", escapedGame);
 };
 
