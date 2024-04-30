a583fdb513d6d4fec0942c2c41dc3c5d5eb128cd
tkawa15 <ttttt.fms5@gmail.com>
2024-04-30 08:05:52 +0000

    deploy: 830a9b6f3a140ccb1bf4105728da5b6c59de3070

--- _nuxt/ContentRendererMarkdown.B814TdVI.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./ContentRendererMarkdown.vue.Ca7sJN1z.js";import"./entry.CiSbMT7V.js";import"./index.BsYmvPZw.js";import"./preview.3G2irJYW.js";import"./index.DvDH6DOc.js";export{o as default};
---
+++ _nuxt/Markdown.CQP5nnYq.js
@@ -0,0 +1,1 @@
+import r from"./ContentSlot.DK46M7vb.js";import{f as o,K as f,j as u,I as c}from"./entry.BqG3TtlG.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:a}=f(),s=u(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
