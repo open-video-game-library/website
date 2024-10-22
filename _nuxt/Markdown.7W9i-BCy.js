be4292cb2c62376bc93d4630511259196fffd748
tkawa15 <ttttt.fms5@gmail.com>
2024-10-22 15:02:11 +0000

    deploy: 68714a92cca0dcc08c684dda0ca386e11b3e963a

---
+++ _nuxt/Markdown.7W9i-BCy.js
@@ -0,0 +1,1 @@
+import r from"./ContentSlot.BBgK_bu6.js";import{f as o,K as f,j as u,I as c}from"./entry.C16Vlsk9.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:a}=f(),s=u(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
