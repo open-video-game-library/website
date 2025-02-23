e797c985debed18532457d57548cfe70e5bef28f
tkawa15 <ttttt.fms5@gmail.com>
2025-02-23 15:40:45 +0900

    ✨ error置き換え

--- error.vue
+++ error.vue
@@ -1,5 +1,10 @@
-<script setup>
+<script setup lang="ts">
+import { useError } from '#imports';
+import GlobalButton from '@/components/global/GlobalButton.vue';
+import { INTERNAL_URL } from '@/constants';
+
 const error = useError();
+console.error(error);
 
 const ERROR_CODE = {
   NOT_FOUND: 404,
@@ -10,26 +15,24 @@ const ERROR_CODE = {
   <NuxtLayout>
     <section class="content-wrapper">
       <div class="content-container">
-        <v-container>
-          <h2>
-            <span v-if="error.statusCode === ERROR_CODE.NOT_FOUND">お探しのページは存在しません。</span>
-            <span v-else>エラーが発生しました。リロードするか、トップページに戻ってやり直してください。</span>
-          </h2>
-          <p class="my-4">
-            Status code: {{ error.statusCode }}
-          </p>
-          <p class="my-4">
-            Message: {{ error.statusMessage }}
-          </p>
-          <v-btn
-            href="/"
-            color="primary"
-            class="my-4"
-          >
-            トップページに戻る
-          </v-btn>
-        </v-container>
+        <h1>
+          <span v-if="error.statusCode === ERROR_CODE.NOT_FOUND">{{ $t('error.notFound') }}</span>
+          <span v-else>{{ $t('error.common') }}</span>
+        </h1>
+        <div class="back-button">
+          <GlobalButton :link="{ to: INTERNAL_URL.ABOUT }">
+            {{ $t('error.back') }}
+          </GlobalButton>
+        </div>
       </div>
     </section>
   </NuxtLayout>
 </template>
+
+<style scoped lang="scss">
+.back-button {
+  display: flex;
+  place-content: center;
+  margin-top: 32px;
+}
+</style>
