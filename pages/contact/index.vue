071281c0fc2914b1dc206bafae97b89bb6b9a40f
tkawa15 <ttttt.fms5@gmail.com>
2025-02-23 13:45:53 +0900

    ✨ contactを置き換え

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,25 +1,50 @@
 <template>
   <div>
     <section class="content-wrapper">
-      <div class="content-container">
-        <VContainer>
-          <h2>{{ $t('contact.title') }}</h2>
-          <aside>{{ $t('contact.description') }}</aside>
-          <VRow
-            justify="center"
-            height="880"
-          >
-            <iframe
-              src="https://docs.google.com/forms/d/e/1FAIpQLScpDBuQV0ca-PTrxrzm4QniH_u5_wF-3SGEHx1FT3SVJGbpqg/viewform?embedded=true"
-              width="640"
-              height="880"
-              frameborder="0"
-              marginheight="0"
-              marginwidth="0"
-            >読み込んでいます…</iframe>
-          </VRow>
-        </VContainer>
+      <div class="content-container contact-wrapper">
+        <h1 class="title">
+          {{ $t('contact.title') }}
+        </h1>
+        <p class="description">
+          {{ $t('contact.description') }}
+        </p>
+        <div class="form">
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLScpDBuQV0ca-PTrxrzm4QniH_u5_wF-3SGEHx1FT3SVJGbpqg/viewform?embedded=true"
+            width="640"
+            height="920"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            class="iframe"
+          >読み込んでいます…</iframe>
+        </div>
       </div>
     </section>
   </div>
 </template>
+
+<style scoped lang="scss">
+.contact-wrapper {
+  > .title {
+    margin-bottom: 32px;
+  }
+
+  > .description {
+    margin-bottom: 32px;
+  }
+
+  > .form {
+    display: flex;
+    place-content: center;
+  }
+
+  > .form > .iframe {
+    height: 920px;
+
+    @media screen and (min-width: 640px) {
+      height: 660px;
+    }
+  }
+}
+</style>
